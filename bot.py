5b475361aa6eccdc8045f441f8ed7b471ce4ad6d
Avi Cohen <ohserr111@walla.com>
2025-05-31 00:43:12 +0300

    פתרתי את כל הקונפליקטים

--- bot.py
+++ bot.py
@@ -184,7 +184,7 @@ def send_summary(message):
 
 import os
 
-if__name__ == '__main__':
+if __name__ == '__main__' :
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
